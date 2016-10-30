a260bb45daeaeda83503c7389987965f4bc794ba
rodrigotejos <showrodrigo@hotmail.com>
2016-10-30 12:56:17 -0200

    Add files via upload
    recebe Bytes e print grafico

---
+++ Plat_Ard/src/plat_ard/Plat_Ard.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plat_ard;
+
+/**
+ *
+ * @author Elisabete
+ */
+public class Plat_Ard {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
